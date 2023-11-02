b9ff9d497beea2e45d4ade02387d100d2dc9e845
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-11-02 23:42:16 +0600

    Added bootstrap, created ingredients interface and INGREDIENTS constant, added ingredients images

---
+++ src/Constants/Constants.ts
@@ -0,0 +1,14 @@
+import meatImage from '../assets/meat.png';
+import cheeseImage from '../assets/cheese.png';
+import saladImage from '../assets/salad.png';
+import baconImage from '../assets/bacon.png';
+import { Ingredients } from '../types';
+
+const INGREDIENTS: Ingredients[] = [
+    {name: 'Meat', cost: 80, img: meatImage},
+    {name: 'Cheese', cost: 50, img: cheeseImage},
+    {name: 'Salad', cost: 10, img: saladImage},
+    {name: 'Bacon', cost: 60, img: baconImage},
+];
+
+export default INGREDIENTS;
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export interface Ingredients {
+    name: string;
+    cost: number;
+    img: string;
+}
